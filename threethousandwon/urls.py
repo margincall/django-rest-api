1bd89a9a0c06c03e57bc22d9198714a4adf8ba93
pelly ryu <hermement@gmail.com>
2016-08-13 05:41:45 +0900

    Django 기본 User, JWT 인증 및 Test(2,3) (#3)
    * Add Django standard User Authentication
    
    참조: DRF API Guide( http://www.django-rest-framework.org/api-guide/authentication/#authentication )
    
    * JSONWebToken Authentication
    
    1. djangorestframework-jwt(1.8.0) 패키지 추가
    2. settings.py의 인증에 jwt 추가
    3. jwt 토큰을 얻기 위한 url 추가
    참고: http://getblimp.github.io/django-rest-framework-jwt/
    
    * Change DRF Permission to IsAuthenticated
    
    인증이 되는지 확인하기 위해 인증된 사용자만 api에 접속가능하도록 변경.
    
    * 인증되지 않은 사용자도 접속 가능해야 하는 부분(아티스트 목록 등)도 있는데,
    ViewSet에 개별적으로 Permission을 부여하자 의도대로 동작하지 않는 문제가 발생.
    아직 Permission을 완벽히 숙지하지 못한 관계로,
    일단 인증 제한을 건 뒤, 차후 필요시 Permission을 조정하는 것이 좋다고 판단했음.
    
    * Test: JWT Authentication
    
    1. JWT 인증 동작 테스트
    (1) REST 프레임워크에서 제공하는 APITestCase 사용
    (2) setUp()메소드는 프레임워크에서 테스트 실행 전 자동 실행됨.
    
    2. Main Page 테스트 수정
    API 접속권한 변경에 따른 테스트 수정.
    !=200보다는 ==401을 체크하는 게 합당함.(404, 400 등을 체크)

--- threethousandwon/urls.py
+++ threethousandwon/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 
 from apps.authentication.views import UserViewSet
 
@@ -29,5 +30,6 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', obtain_jwt_token, name='obtain_jwt_token'),
 ]
