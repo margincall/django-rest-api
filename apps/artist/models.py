51e7bc9067437e168091d7d43c5a1ec89b4e33b0
pelly ryu <ryuch.hyuniv@gmail.com>
2016-08-17 02:19:49 +0900

    Create Artist model
    with auto-generated migration codes
    manage.py: `makemigrations | migrate`

--- apps/artist/models.py
+++ apps/artist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    nickname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nickname
