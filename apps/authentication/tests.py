6fdf17c4509e52a6e08d834322d0fb285e709f0d
pelly ryu <ryuch.hyuniv@gmail.com>
2016-08-20 01:10:13 +0900

    Test: User API test with Custom User

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from rest_framework.test import APITestCase
 
 from apps.authentication.models import User
 
@@ -18,3 +21,19 @@ class UserModelTest(TestCase):
 
     def test_올바르지_않은_Email_User_생성할_때_실패(self):
         self.assertRaises(ValueError, lambda: User.objects.create_user('is#this.email', 'password'))
+
+
+class UserAPITests(APITestCase):
+
+    def test_API_에서_생성된_User_객체를_받아오는지_확인(self):
+        email = 'green@day.com'
+        User.objects.create_user(email=email, password='password')
+
+        # JWT Authentication
+        response = self.client.post(reverse('obtain_jwt_token'),
+                                    {"email": 'green@day.com', "password": 'password'},
+                                    format='json')
+        token = response.data['token']
+
+        response = self.client.get("/users/", {}, HTTP_AUTHORIZATION='JWT {}'.format(token), format='json')
+        self.assertIn(email, [i['email'] for i in response.data], msg=response.data)
