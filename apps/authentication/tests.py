af9dc5b1a98453d0b3987766fb054321d247c777
pelly ryu <ryuch.hyuniv@gmail.com>
2016-08-20 00:18:04 +0900

    Custom User with Tests
    아직 인증 체계에는 등록 안 했습니다.
    has_perm()과 has_module_perm()에서 inspection이 노란 줄을 긋는데,
    지금은 구현이 안 되었을 뿐이라 일단 냅뒀습니다.
    
    참고 : https://docs.djangoproject.com/en/1.10/topics/auth/customizing/

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.authentication.models import User
+
+
+class UserModelTest(TestCase):
+    def test_User_생성_및_문자열_표현_확인(self):
+        email = 'email@email.com'
+        user = User.objects.create_user(email, 'password')
+        self.assertEqual(email, str(user))
+
+    def test_Superuser_생성_및_권한_확인(self):
+        user = User.objects.create_superuser('admin@email.com', 'password')
+        self.assertTrue(user.is_admin)
+
+    def test_Email_없이_User_생성할_때_실패(self):
+        self.assertRaises(ValueError, lambda: User.objects.create_user('', 'password'))
+
+    def test_올바르지_않은_Email_User_생성할_때_실패(self):
+        self.assertRaises(ValueError, lambda: User.objects.create_user('is#this.email', 'password'))
